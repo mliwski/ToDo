5072424210166e45597ddebda2f71873da92f5e2
mliwski <matias.liwski@avantrip.com>
2015-03-11 13:45:15 -0300

    Added connect server, can't get livereload to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,11 @@
 module.exports = function(grunt) {
 
 	var config = grunt.file.readJSON('config.json');
-	console.log("ACA" + config);
-	console.log(config.clientId);
+	var pkg = grunt.file.readJSON('package.json');
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        replace: {
-			configuration: grunt.file.readJSON('config.json'),
+		replace: {
             config: {
                 src: ['src/index.html'],
                 dest: 'build/index.html',
@@ -17,11 +14,41 @@ module.exports = function(grunt) {
                     to: config.clientId
                 }]
             }
+        },
+		connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    port: 80,
+                    base: 'build',
+                    keepalive: true,
+                    livereload: true,
+					open: true
+                }
+            }
+        },
+        watch: {
+			options: {
+				livereload: 80,
+			},
+			html: {
+				files: ['src/*.html'],
+				tasks: ['replace'],
+			}
         }
     });
+	
+	grunt.registerTask('project_banner', 'Log project banner', function() {
+		grunt.log.writeln("");
+		grunt.log.writeln(" ########################");
+		grunt.log.writeln(" #	" + pkg.name + " V" + pkg.version + "	#");
+		grunt.log.writeln(" ########################");
+	});
 
     grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['replace']);
+    grunt.registerTask('default', ['project_banner', 'replace', 'connect', 'watch']);
 };
