9c59695fe7941fbf396bfcce609d25abb61e238d
Matias Liwski <matias.liwski@gmail.com>
2015-04-04 16:59:47 -0300

    Finally could make a mock with promises

--- test/unit/lists/listsControllerSpecs.js
+++ test/unit/lists/listsControllerSpecs.js
@@ -18,16 +18,22 @@ angular.module('listServiceModuleMock', []).
 
 /* jasmine specs for lists controllers go here */
 describe('List Controller', function() {
-
+	var scope = {};
+	
     beforeEach(module('todoApp'));
     beforeEach(module('listServiceModuleMock'));
+	beforeEach(inject(function ($compile, $rootScope) {
+        scope = $rootScope.$new();
+    }));
 
 
 
-    it('should create 3 list', inject(function($controller,_listServiceMock_) {
-        var scope = {};
+
+    it('should create 3 list', inject(function($controller,_listServiceMock_,$rootScope) {
         var ctrl = $controller('ListController', {$scope:scope, 'List':_listServiceMock_});
 
+		scope.$digest();
+		
         expect(scope.lists.length).toBe(2);
     }));
 
